1e8316ded918cd9ccf75aa49e1c13d2f4ec114d3
popor <908891024@qq.com>
2020-10-12 09:17:58 +0800

    歌单列表搜索栏颜色

--- LM/Fun/SongSection/SongListDetailVC/View/SongListDetailVC.m
+++ LM/Fun/SongSection/SongListDetailVC/View/SongListDetailVC.m
@@ -162,13 +162,8 @@
     oneTV.estimatedSectionHeaderHeight = 0;
     oneTV.estimatedSectionFooterHeight = 0;
     
-#if TARGET_OS_MACCATALYST
-    oneTV.backgroundColor = PColorTVBG;
-    oneTV.separatorColor  = [UIColor grayColor];
-    
-#else
-    
-#endif
+    oneTV.backgroundColor = App_bgColor1;
+    oneTV.separatorColor  = App_separatorColor;
     
     [self.view addSubview:oneTV];
     
@@ -241,12 +236,10 @@
     if (!searchBar) {
         searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, PSCREEN_SIZE.width, 44+10)];
 #if TARGET_OS_MACCATALYST
-        [searchBar setBackgroundImage:[UIImage imageFromColor:PRGB16(0XF0F0F0) size:CGSizeMake(1, 1)] forBarPosition:UIBarPositionAny barMetrics:UIBarMetricsDefault]; //此处使底部线条颜色为红色
+        searchBar.barTintColor = App_bgColor2;
         searchBar.tintColor = [UIColor blackColor];
-        //searchBar.searchTextField.backgroundColor = PRGB16F(0Xe1e1e1, 1);
-        searchBar.searchTextField.backgroundColor = [UIColor lightGrayColor];
-        
-        searchBar.searchTextField.textColor = [UIColor blackColor];
+        searchBar.searchTextField.backgroundColor = [UIColor tertiarySystemBackgroundColor];
+        searchBar.searchTextField.textColor       = App_textNColor;
         
 #else
         [searchBar setBackgroundImage:[UIImage imageFromColor:[UIColor clearColor] size:CGSizeMake(1, 1)] forBarPosition:UIBarPositionAny barMetrics:UIBarMetricsDefault]; //此处使底部线条颜色为红色
