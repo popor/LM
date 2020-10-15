a9fbc03bb960dbcdcaaed7a50adf0bc30ea486c1
popor <908891024@qq.com>
2020-10-15 15:07:08 +0800

    加载歌词, 首先检查本地是否有lrc

--- LM/Fun/MusicPlay/MusicPlayBar/MusicPlayBar.m
+++ LM/Fun/MusicPlay/MusicPlayBar/MusicPlayBar.m
@@ -748,6 +748,18 @@ static CGFloat MPBTimeLabelWidth1 = 57;
     self.musicLyricArray = nil;
     self.lastMusicTitle  = self.currentItem.musicName;
     
+    // 先排查是否有保存
+    NSString * lrcPath = [LrcTool lycPath:self.currentItem.fileNameDeleteExtension];
+    NSData   * lrcData = [NSData dataWithContentsOfFile:lrcPath];
+    if (lrcData) {
+        NSString * lrc = [[NSString alloc] initWithData:lrcData encoding:NSUTF8StringEncoding];
+        [self parseLrc:lrc];
+    } else {
+        [self requestLrcKugou];
+    }
+}
+
+- (void)requestLrcKugou {
     @weakify(self);
     [LrcKuGou getLrcList:self.currentItem finish:^(LrcKugouListEntity * _Nullable listEntity) {
         @strongify(self);
@@ -755,54 +767,56 @@ static CGFloat MPBTimeLabelWidth1 = 57;
             LrcKugouListUnitEntity * ue = listEntity.lrcArray.firstObject;
             [LrcKuGou getLrcDetail:self.currentItem id:ue.id accesskey:ue.accesskey finish:^(NSString *string) {
                 @strongify(self);
-                NSDictionary * originDic = [LrcTool parselrc:string];
-                
-                NSMutableArray * originArray = [NSMutableArray new];
-                NSArray * timeTextArray = originDic.allKeys;
-                for (NSString * timeText in timeTextArray) {
-                    
-                    LrcDetailEntity * entity = [LrcDetailEntity new];
-                    entity.timeText = timeText;
-                    entity.lrc      = originDic[timeText];;
-                    
-                    NSRange range = [timeText rangeOfString:@":"];
-                    NSInteger mm  = [timeText substringToIndex:range.location].integerValue;
-                    NSInteger ss  = [timeText substringFromIndex:range.location +1].integerValue;
-                    entity.time   = mm*60 +ss;
-                    
-                    [originArray addObject:entity];
-                }
-                
-                self.musicLyricArray = [originArray sortedArrayUsingComparator:^NSComparisonResult(LrcDetailEntity * obj1, LrcDetailEntity * obj2) {
-                    //return [obj1.time compare:obj2.time]; //升序
-                    return obj1.time<obj2.time ? NSOrderedAscending:NSOrderedDescending;
-                }];
-                
-                
-                NSMutableDictionary * tempDic = [NSMutableDictionary new];
-                NSInteger count = self.musicLyricArray.count;
-                for (NSInteger row = 0; row<count; row++) {
-                    LrcDetailEntity * entity = self.musicLyricArray[row];
-                    entity.row = row;
-                    
-                    tempDic[entity.timeText] = entity;
-                }
-                self.musicLyricDic = tempDic;
-                
-                NSDictionary * dic = @{@"lrcArray":self.musicLyricArray};
-                [MGJRouter openURL:MUrl_updateLrcData withUserInfo:dic completion:nil];
-                
-                // for (LrcDetailEntity * entity in self.musicLyricArray) {
-                //     [NSAssistant NSLogEntity:entity];
-                //     NSLog(@"\n ");
-                // }
+                [self parseLrc:string];
             }];
         } else {
             [MGJRouter openURL:MUrl_updateLrcData];
         }
+    }];
+}
+
+- (void)parseLrc:(NSString *)lrc {
+    NSDictionary * originDic = [LrcTool parselrc:lrc];
+    
+    NSMutableArray * originArray = [NSMutableArray new];
+    NSArray * timeTextArray = originDic.allKeys;
+    for (NSString * timeText in timeTextArray) {
         
+        LrcDetailEntity * entity = [LrcDetailEntity new];
+        entity.timeText = timeText;
+        entity.lrc      = originDic[timeText];;
+        
+        NSRange range = [timeText rangeOfString:@":"];
+        NSInteger mm  = [timeText substringToIndex:range.location].integerValue;
+        NSInteger ss  = [timeText substringFromIndex:range.location +1].integerValue;
+        entity.time   = mm*60 +ss;
+        
+        [originArray addObject:entity];
+    }
+    
+    self.musicLyricArray = [originArray sortedArrayUsingComparator:^NSComparisonResult(LrcDetailEntity * obj1, LrcDetailEntity * obj2) {
+        //return [obj1.time compare:obj2.time]; //升序
+        return obj1.time<obj2.time ? NSOrderedAscending:NSOrderedDescending;
     }];
     
+    
+    NSMutableDictionary * tempDic = [NSMutableDictionary new];
+    NSInteger count = self.musicLyricArray.count;
+    for (NSInteger row = 0; row<count; row++) {
+        LrcDetailEntity * entity = self.musicLyricArray[row];
+        entity.row = row;
+        
+        tempDic[entity.timeText] = entity;
+    }
+    self.musicLyricDic = tempDic;
+    
+    NSDictionary * dic = @{@"lrcArray":self.musicLyricArray};
+    [MGJRouter openURL:MUrl_updateLrcData withUserInfo:dic completion:nil];
+    
+    // for (LrcDetailEntity * entity in self.musicLyricArray) {
+    //     [NSAssistant NSLogEntity:entity];
+    //     NSLog(@"\n ");
+    // }
 }
 
 - (void)showBigIVAction {
